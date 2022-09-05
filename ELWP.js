7f0f74421b8b0f6dd1e8900cf31755cc923f9e46
mudgepj <110132410+mudgepj@users.noreply.github.com>
2022-09-05 17:28:31 +0100

    Update ELWP.js

--- ELWP.js
+++ ELWP.js
@@ -100,7 +100,7 @@ function LoadPage (rstrKey, rstrGet, rboolReload) {
 }
 function LoadPageNumber(rintFile, rstrGet, rboolReload) {
 	alert (rintFile);
-	var xhttp = GetXhttp("main");
+/*	var xhttp = GetXhttp("main");
 	xhttp.open("POST", marrMenus[rintFile][1], true);
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	if (marrMenus[rintFile][1] == "Diary/Events.php")
@@ -140,6 +140,7 @@ function LoadPageNumber(rintFile, rstrGet, rboolReload) {
 			'value' : rintFile
 		}
 	);
+*/	
 //	HideMobileDropdown();
 //	document.getElementById("idMobileMenu").style.display = "none";
 }

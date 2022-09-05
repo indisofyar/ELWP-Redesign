0b24194c35a97a128485505afcb0ef25364eef7c
mudgepj <110132410+mudgepj@users.noreply.github.com>
2022-09-05 17:30:12 +0100

    Update ELWP.js

--- ELWP.js
+++ ELWP.js
@@ -99,7 +99,7 @@ function LoadPage (rstrKey, rstrGet, rboolReload) {
 	LoadPageNumber (intFile, rstrGet, rboolReload);
 }
 function LoadPageNumber(rintFile, rstrGet, rboolReload) {
-	alert (rintFile);
+	alert (marrMenus[rintFile][1]);
 /*	var xhttp = GetXhttp("main");
 	xhttp.open("POST", marrMenus[rintFile][1], true);
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");

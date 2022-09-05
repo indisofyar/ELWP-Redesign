bac2495b5e0b644776164d3453127c7592fd0b77
mudgepj <110132410+mudgepj@users.noreply.github.com>
2022-09-05 17:10:08 +0100

    Update ELWP.js

--- ELWP.js
+++ ELWP.js
@@ -194,6 +194,7 @@ function RedoBanner (){
 	document.getElementById("idIcon").style.width = fltStraightLineLookup (cintMinWidth, cintMaxWidth, cfltMinLogoWidth, cfltMaxLogoWidth, window.innerWidth) + "%";
 	document.getElementById("idBanner").style.width = fltStraightLineLookup (cintMinWidth, cintMaxWidth, cfltMinMenuWidth, cfltMaxMenuWidth, window.innerWidth) + "%";
 */
+	alert ('here');
 	var strHtml = "<ul>";
 	var strHtml1 = "<ul>";
 	var strHtml2 = "<ul>";
